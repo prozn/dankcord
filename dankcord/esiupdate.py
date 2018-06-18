fcea963e7934061e3fc211757d6fa4396243c1f8
Prozn Zanjoahir <matt@quickmind.co.uk>
2018-06-18 14:11:01 +0100

    test

--- dankcord/esiupdate.py
+++ dankcord/esiupdate.py
@@ -2,46 +2,35 @@
 # -*- coding: utf-8 -*-
 
 import time
-import pony
-from esipy import EsiApp
-from esipy import EsiClient
-from esipy import EsiSecurity
-from esipy.cache import FileCache
 import configparser
+
+from .lib.database import *
+from .lib.esi import ESI
 
 config = configparser.SafeConfigParser()
 
 def start(configpath="."):
-    global app, security, esi
     config.read("%s/config.ini" % configpath)
-    cache = FileCache(path=config.get('esi','cache'))
-    esi_app = EsiApp(cache=cache)
-    app = esi_app.get_latest_swagger
-    security = EsiSecurity(
-        app=app,
-        redirect_uri='http://localhost/oauth-callback', # This doesnt matter
-        client_id=config.get('esi','client_id'),
-        secret_key=config.get('esi','secret_key'),
+    corp = ESI(
+        config.get('esi','client_id'),
+        config.get('esi','secret_key'),
+        config.get('esi','refresh_token'),
+        config.get('esi','cache'),
+        'corp'
     )
-    esi = EsiClient(
-        retry_requests=True,  # set to retry on http 5xx error (default False)
-        headers={'User-Agent': 'Discord bot by Prozn: https://github.com/prozn/dankcord'},
-        security=security
-    )
-    security.update_token({
-        'access_token': '',  # leave this empty
-        'expires_in': -1,  # seconds until expiry, so we force refresh anyway
-        'refresh_token': config.get('esi','refresh_token')
-    })
-    security.refresh()
-
     while True:
         if get_contracts():
             time.sleep(60)
         else:
             time.sleep(1)
 
-def get_contracts():
+def get_contracts(corp):
+    contracts = corp.esi.corp_contracts(config.get('corporation','corporation_id'))
+    for contract in contracts.data:
+        print contract
+        #check_contract(contract)
+
+def get_contracts_old():
     op = app.op['get_corporations_corporation_id_contracts'](
         corporation_id=config.get('corporation','corporation_id')
     )
