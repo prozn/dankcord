077830d119049013917d99ff6936ec6aea7fec0f
Prozn Zanjoahir <matt@quickmind.co.uk>
2018-06-18 14:12:50 +0100

    test

---
+++ dankcord/lib/database.py
@@ -0,0 +1,49 @@
+from pony.orm import *
+
+db = Database()
+db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
+
+class Contract(db.Entity):
+	contract_id = PrimaryKey(int, 20)
+	acceptor_id = Optional(int, 20) # monitor for changes
+	assignee_id = Optional(int, 20) 
+	availability = Required(str,50) # filter = "personal" only
+	collateral = Optional(Decimal, 20, 2)
+	date_accepted = Optional(datetime)
+	date_completed = Optional(datetime)
+	date_expired = Optional(datetime)
+	date_issued = Required(datetime)
+	days_to_complete = Optional(int, 3)
+	end_location_id = Optional(int, 20)
+	for_corporation = Optional(bool)
+	issuer_corporation_id = Optional(int, 20)
+	issuer_id = Optional(int, 20)
+	price = Optional(Decimal, 20, 2)
+	reward = Optional(Decimal, 20, 2)
+	start_location_id = Optional(int, 20)
+	status = Optional(str,50) # monitor for changes
+	title = Optional(str,255)
+	contract_type = Required(str, 50) # filter = "courier" only
+	volume = Optional(Decimal, 20, 2)
+
+class Messages(db.Entity):
+	id = PrimaryKey(int, auto=True)
+	contract_id = Required(Contract)
+	reason = Required(str, 50)  # NEW, ACCEPTED, EXPIRING_SOON, COMPLETED, EXPIRED
+
+db.generate_mapping(create_tables=True)
+
+@db_session
+async def contract_exists(contract_id):
+	try:
+		contract = Contract[contract_id]
+		return true
+	except ObjectNotFound:
+		return false
+
+@db_session
+async def update_contract(c):
+	if contract_exists(c.contract_id):
+		Contract[c.contract_id].set(**c)
+	else:
+		Contract(**c)
---
+++ dankcord/lib/esi.py
@@ -0,0 +1,57 @@
+from esipy import EsiApp
+from esipy import EsiClient
+from esipy import EsiSecurity
+from esipy.cache import FileCache
+
+class ESI:
+    def __init__(self,client_id,secret_key,refresh_token,cache_path,prefix='esipy'):
+        self.client_id = client_id
+        self.secret_key = secret_key
+        self.refresh_token = refresh_token
+        self.cache_path = cache_path
+        self.prefix = prefix
+
+        self._start()
+
+    def _start(self):
+        self.cache = FileCache(path=self.cache_path)
+        self.esi_app = EsiApp(cache=self.cache, cache_prefix=self.prefix)
+        self.app = esi_app.get_latest_swagger
+        self.security = EsiSecurity(
+            app=self.app,
+            redirect_uri='http://localhost/oauth-callback', # This doesnt matter
+            client_id=self.client_id,
+            secret_key=self.secret_key,
+        )
+        self.esi = EsiClient(
+            retry_requests=True,  # set to retry on http 5xx error (default False)
+            headers={'User-Agent': 'Discord bot by Prozn: https://github.com/prozn/dankcord'},
+            security=self.security
+        )
+        security.update_token({
+            'access_token': '',  # leave this empty
+            'expires_in': -1,  # seconds until expiry, so we force refresh anyway
+            'refresh_token': self.refresh_token
+        })
+        security.refresh()
+
+    def character_info(self, character_id):
+        op = self.app.op['get_characters_character_id'](
+            character_id=character_id
+        )
+        character = self.esi.request(op,raise_on_error=True)
+        return character
+
+    def character_name(self, character_id):
+        character = self.character_info(character_id)
+        return character.name
+
+    def corp_contracts(self, corporation_id):
+        op = self.app.op['get_corporations_corporation_id_contracts'](
+            corporation_id=corporation_id
+        )
+        contracts = esi.request(op,raise_on_error=True)
+        return contracts
+
+    def personal_contracts(self):
+        raise NotImplementedError
