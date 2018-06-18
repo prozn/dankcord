967ba53c01ec9aaaa49a2fca0e1e02d090267cb6
Prozn Zanjoahir <matt@quickmind.co.uk>
2018-06-18 21:32:49 +0100

    fix

--- dankcord/lib/database.py
+++ dankcord/lib/database.py
@@ -83,7 +83,7 @@ def add_message(contract_id,reason):
 
 @db_session
 def pop_message(delete=True):
-	mess = Message.select(lambda m: m.sent == True).order_by(lambda m: asc(m.id))[:1]
+	mess = Message.select(lambda m: m.sent == True).order_by(lambda m: m.id)[:1]
 	if len(mess) > 0:
 		item = mess[0]
 		if delete:
